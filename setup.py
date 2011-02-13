31dc05a348be7b49b12b8608736d2fc2a6f4b38b
Thierry Bertin-Mahieux <tb2332@columbia.edu>
2011-02-13 13:58:10 -0500

    setup seems to work

---
+++ setup.py
@@ -0,0 +1,14 @@
+"""
+Setup script for the pyMusixMatch module
+"""
+
+from distutils.core import setup
+
+setup(name='musixmatch',
+      version='1.0',
+      description='Python Wrapper for the MusixMatch API',
+      author='A. Anglade and T. Bertin-Mahieux',
+      author_email='amelie.anglade@gmail.com',
+      url='https://github.com/utstikkar/pyMusiXmatch',
+      packages=['musixmatch'],
+     )

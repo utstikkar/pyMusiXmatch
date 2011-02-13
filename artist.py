5e23a5b09408109f54d87d7b6d07cf2c4e23f549
Amélie Anglade <amelie.anglade@gmail.com>
2011-02-12 19:27:07 -0500

    Added artist.py

---
+++ artist.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+"""
+track.py
+ by A. Anglade and T. Bertin-Mahieux
+"""
+
+import os
+import sys
+import time
+import datetime
+
+class Track(object):
+	"""
+	Class to query the musixmatch API artists
+	"""
+	
+	#track.get in API
+	def __init__(self):
+		"""
+		Takes a musixmatch ID or musicbrainz id or echo nest track id
+		"""
+		
+		self._tid = ''
+		return
+		
+	#track.lyrics.get in API	
+	def lyrics(self):
+		"""
+		"""
+		raise NotImplementedError
+		
+	#track.subtitle.get	in API
+	def subtitles(self):
+		raise NotImplementedError
+		
+	#track.chart.get in API	
+	def charts(self):
+		raise NotImplementedError
+		
+		
+#track.search in API		
+def search():
+	track_list = list()
+	
+	return track_list
